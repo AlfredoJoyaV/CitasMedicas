8d0e1b8b105fcaf509b53c322181a95e5cdadd72
Erick Joya <ejoyavaz@gmail.com>
2025-07-27 22:14:58 -0600

    ::tada::Iniciado

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,40 @@
+import { Form } from '../components/From/Form';
+import { getDictionary } from "./dictionaries";
+
+export default async function Home({params}: {params:Promise<{lang: 'es-ES' | 'en-US' }>}) {
+  const {lang} = await params
+  const dict = await getDictionary(lang)
+  return (
+    <section className="min-h-screen bg-gradient-to-br from-black to-purple-300">
+      <header className="bg-white shadow-md p-4">
+        <div className="mx-auto max-w-7xl">
+          <h1 className=" text-center text-3xl font-bold text-gray-900">
+            {dict.HomePage.title}
+          </h1>
+          <p className="text-center mt-2 text-gray-700">
+            {dict.HomePage.description}
+          </p>
+        </div>
+      </header>
+
+      <main className=" mx-auto max-w-4xl px-6 py8">
+        <Form formName={dict.ComponentsForm.formName}
+        formLastName={dict.ComponentsForm.formLastName}
+        formDateOfBorn={dict.ComponentsForm.formDateOfBorn}
+        formGender={dict.ComponentsForm.formGender}
+        formCellPhone={dict.ComponentsForm.formCellPhone}
+        fomrEmail={dict.ComponentsForm.fomrEmail}
+        fomrDate={dict.ComponentsForm.fomrDate}
+        fomrHour={dict.ComponentsForm.fomrHour} 
+        placeholderName={dict.ComponentsForm.placeholderName} 
+        placeholderLastName={dict.ComponentsForm.placeholderLastName} 
+        placeholderDateOfBorn={dict.ComponentsForm.placeholderDateOfBorn} 
+        placeholderGender={dict.ComponentsForm.placeholderGender} 
+        placeholderCellPhone={dict.ComponentsForm.placeholderCellPhone} 
+        placeholderEmail={dict.ComponentsForm.placeholderEmail} 
+        placeholderDate={dict.ComponentsForm.placeholderDate} 
+        placeholderHour={dict.ComponentsForm.placeholderHour}/>
+      </main>
+    </section>
+  );
+}
---
+++ src/app/components/From/Form.tsx
@@ -0,0 +1,171 @@
+'use client';
+import {useForm, SubmitHandler} from 'react-hook-form'
+
+type FromProps = {
+    formName: string;
+    formLastName: string;
+    formDateOfBorn: string;
+    formGender: string;
+    formCellPhone: string;
+    fomrEmail: string;
+    fomrDate: string;
+    fomrHour: string;
+    // Placeholders
+    placeholderName: string;
+    placeholderLastName: string;
+    placeholderDateOfBorn: string;
+    placeholderGender: string;
+    placeholderCellPhone: string;
+    placeholderEmail: string;
+    placeholderDate: string;
+    placeholderHour: string
+
+}
+
+type Inputs = {
+  name: string;
+  lastname: string;
+  dateofborn: Date;
+  gender: string;
+  cellphone: number;
+  email: string;
+  date: Date;
+  hour: string
+}
+
+export const Form = (FromProps:FromProps) => {
+  const {register, handleSubmit, watch, formState:{errors}} = useForm<Inputs>()
+  
+  console.log("ERRORES EN FORM", errors);
+  const onSubmit = (data:Inputs) => {
+    
+    console.log("Formulario enviado", data);
+  }
+
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6 max-w-2x1 mx-auto mt-6">
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+            <div className='grid grid-cols-1 gap-4'>
+              <div>
+                <label htmlFor='name' className="italic font-light text-2xl text-black">
+                  {FromProps.formName}
+                </label>
+                <input 
+                  type="text"
+                  id='name'
+                  {...register("name", {required: "El nombre es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  placeholder={FromProps.placeholderName}
+                />
+              </div>
+              <div>
+                <label htmlFor='lastname' className="italic font-light text-2xl text-black">
+                  {FromProps.formLastName}
+                </label>
+                <input 
+                  type="text"
+                  id='lastname'
+                  {...register("lastname", {required: "El apellido es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  placeholder={FromProps.placeholderLastName}
+                />
+              </div>
+              <div>
+                <label htmlFor='dateofborn' className="italic font-light text-2xl text-black">
+                  {FromProps.formDateOfBorn}
+                </label>
+                <input 
+                  type="date"
+                  id='dateofborn'
+                  {...register("dateofborn", {required: "El fecha de nacimiento es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  placeholder={FromProps.placeholderDateOfBorn}
+                />
+              </div>
+              <div>
+                <label htmlFor='gender' className="italic font-light text-2xl text-black">
+                  {FromProps.formGender}
+                </label>
+                <select
+                  id='gender'
+                  {...register("gender", {required: "El genero es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  defaultValue=""
+                >
+                  <option value="" disabled>{FromProps.placeholderGender}</option>
+                  <option value="masculino">Masculino</option>
+                  <option value="femenino">Femenino</option>
+                </select>
+              </div>
+
+              <div>
+                <label htmlFor='cellphone' className="italic font-light text-2xl text-black">
+                  {FromProps.formCellPhone}
+                </label>
+                <input 
+                  type="number"
+                  id='cellphone'
+                  {...register("cellphone", {required: "El numero de celular es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  placeholder={FromProps.placeholderCellPhone}
+                />
+              </div>
+              <div>
+                <label htmlFor='email' className="italic font-light text-2xl text-black">
+                 {FromProps.fomrEmail} 
+                </label>
+                <input 
+                  type="email"
+                  id='email'
+                  {...register("email", {required: "El correo es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500'
+                  placeholder={FromProps.placeholderEmail}
+                />
+              </div>
+              <div>
+                <label htmlFor='date' className="italic font-light text-2xl text-black">
+                  {FromProps.fomrDate}
+                </label>
+                <input 
+                  type="date"
+                  id='date'
+                  {...register("date", {required: "El dia de la cita es obligatorio"})}
+                  className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500' 
+                  placeholder={FromProps.placeholderDate}
+                />
+              </div>
+              <div>
+                <label htmlFor='hour' className="italic font-light text-2xl text-black">
+                  {FromProps.fomrHour}
+                </label>
+              <select
+                id="hour"
+                {...register("hour", {required: "La hora de la cita es obligatorio"})}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 text-gray-500"
+                defaultValue=""
+              >
+                <option value="" disabled>{FromProps.placeholderHour}</option>
+                <option value="09:00">09:00</option>
+                <option value="10:00">10:00</option>
+                <option value="11:00">11:00</option>
+                <option value="12:00">12:00</option>
+                <option value="13:00">13:00</option>
+                <option value="14:00">14:00</option>
+                <option value="15:00">15:00</option>
+                <option value="16:00">16:00</option>
+                <option value="17:00">17:00</option>
+              </select>
+              </div>
+              <div className='flex justify-center'>
+                <button 
+                  type='submit'
+                  className='bg-purple-500 text-white px-6 py-2 rounded-md hover:bg-purple-600 transition-colors duration-300'
+                >
+                  enviar
+                </button>
+              </div>
+            </div>
+        </form>
+    </div>
+  )
+}
